3473a7e546f6b1c79c4d371852dd3ecfe8c7f73d
abrarum <abraracs@gmail.com>
2022-08-07 14:02:07 +0200

    fix - copied status now changes with a new input

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,6 +23,7 @@ const Home: NextPage = () => {
   const getShortenLink = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setShortURL("");
+    setCopyStatus("");
     const urlPattern =
       /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
     // check if regex is true
